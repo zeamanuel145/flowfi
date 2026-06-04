196b45df3db6ffe5e16db5afa2f6a1105927cf72
zeamanuel <zeamanuel1994@gmail.com>
2026-06-04 19:28:16 +0300

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: { allowedOrigins: ['localhost:3000'] },
-  },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
